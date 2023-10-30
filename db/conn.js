3d755c908aedd44ea4d2535902b400e6f77fd7ed
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2023-10-30 13:38:46 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
-const conn = new Sequelize('establishment', 'root', '16042003', {
+const conn = new Sequelize('establishment', 'admin', 'vrdo1604', {
     dialect: 'mysql',
-    host: 'establishment.cdme6glddevi.sa-east-1.rds.amazonaws.com'
+    host: 'mydatabases.cdme6glddevi.sa-east-1.rds.amazonaws.com'
 })
 module.exports = conn
